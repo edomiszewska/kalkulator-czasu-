baf61e021b3c7bc4a58b88de725f5ff24c131b23
emiiiiiiiiiiiiiii <102521843+edomiszewska@users.noreply.github.com>
2024-11-14 14:51:14 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+const przyciskdni = document.querySelector('#dni');
+const kalendarz = document.querySelector('#kalendarz');
+const wydarzenie = document.querySelector('#wydarz');
+const wynik = document.querySelector('#wynik');
+const kalen= document.querySelector('#kalen');
+const cos=1;
+
+przyciskdni.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if (wydarzenie.value) {
+        const dataDocelowa = new Date(wydarzenie.value);
+        const dzisdata = new Date();
+        const roznicaCzasu = dataDocelowa - dzisdata;
+        const roznicaDni = Math.ceil(roznicaCzasu/(1000*60*60*24));
+        wynik.innerHTML = `pozostało ${roznicaDni} dni do wybranej daty.`;
+
+    } else {
+        wynik.innerHTML = "wybierz wydarzenie z listy!!!!";
+    }
+});
+
+kalen.addEventListener('click',(evt)=>
+{
+    kalendarz.classList.toggle('hidden');
+
+    if (kalen.innerHTML === "wyświetl kalendarz") {
+        kalen.innerHTML = "Ukryj kalendarz";
+    } else {
+        kalen.innerHTML = "wyświetl kalendarz";
+        tablica.innerHTML=" "
+    }
+
+
+}
+)
+kalendarz.addEventListener('change', (evt) => {
+    if (kalendarz.value) {
+        const dataDocelowa = new Date(wydarzenie.value);
+        const dzisdata = new Date(kalendarz.value);
+        const roznicaCzasu = dataDocelowa - dzisdata;
+        const roznicaDni = Math.ceil(roznicaCzasu/(1000*60*60*24));
+        wynik.innerHTML = `pozostało ${roznicaDni} dni do wybranej daty.`;
+        
+    }
+});
